b24efee0581f12d155267d94d50d70fb779cf266
Baris Yuksel <barisyuksel@barisyuksel-macbookpro.roam.corp.google.com>
2016-05-17 01:53:02 -0700

    javascript

---
+++ temp.js
@@ -0,0 +1,12 @@
+var rect = element.getBoundingClientRect();
+console.log(rect.top, rect.right, rect.bottom, rect.left);
+
+  var mysize = document.getElementById("introtext").offsetWidth;
+  var t = (screen.width/11);
+  document.getElementById("introtext").style.marginLeft = t + "px";
+
+  var scsize = screen.width - 2*t;
+  console.log(mysize, scsize, t, (mysize - scsize)/2,(screen.width - mysize)/-2);
+  document.getElementById("introtext").style.marginRight = (mysize - scsize)/2 + "px";
+  document.getElementById("introtext").style.marginLeft = (screen.width - mysize)/-2+ "px";
+  document.getElementById("introtext").style.marginRight = (screen.width - mysize)/-2+ "px";
